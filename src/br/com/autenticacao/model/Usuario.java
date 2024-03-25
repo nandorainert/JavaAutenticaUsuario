587fbcc40baf616bf924148fa79b34d5d1f7c1cc
Ferdinando Rainert <ferdinandorainert24@gmail.com>
2024-03-25 20:25:22 -0300

    colocando os tipos na classe Usuario

--- src/br/com/autenticacao/model/Usuario.java
+++ src/br/com/autenticacao/model/Usuario.java
@@ -1,5 +1,67 @@
 package br.com.autenticacao.model;
 
 public class Usuario {
+	
+	int id;
+	String nome;
+	String email;
+	String senha;
+	Boolean ativo;
+	
+	public Usuario() {
+	}
+	
+	public Usuario(int id) {
+		this.id = id;
+	}
 
+	public Usuario(int id, String nome, String email, String senha, Boolean ativo) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.ativo = ativo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
 }
